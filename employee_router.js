aae047caa204aafb680729543dd7147303135563
AFREEN <afreenbanu0626@gmail.com>
2021-09-06 17:16:58 +0530

    second

--- employee_router.js
+++ employee_router.js
@@ -1,7 +1,39 @@
+const e = require('express');
 var express=require('express')
 var router= express.Router();
 
-var employee = [];
+var employee = [{
+    fullname: 'ravi',
+    email: 'ravi@gmail.com',
+    mobile: 1122,
+    city: 'chennai',
+    profession: 'manager',
+    industry: 'it'
+},
+{
+    fullname: 'sateesh',
+    email: 'sat@gmail.com',
+    mobile: 5566,
+    city: 'ooty',
+    profession: 'lead',
+    industry: 'software'
+},
+{
+    fullname: 'praveen',
+    email: 'prav@gmail.com',
+    mobile: 2233,
+    city: 'hyd',
+    profession: 'trainer',
+    industry: 'Research and Development'
+},
+{
+    fullname: 'Nidhi',
+    email: 'nid@gmail.com',
+    mobile: 9988,
+    city: 'bengaluru',
+    profession: 'HR',
+    industry: 'HRD'
+}];
 router.get("/regemployee",function(req,res){
     res.sendFile(__dirname+"/employee.html");    
 })
@@ -17,4 +49,17 @@ router.post("/employeereg",function(req,res){
     res.send("employee registraiton successful")    
 })
 
+router.get("/details/:id",function(req,res){
+    var emp = employee.find((e,i)=>{
+        if(e.mobile===Number(req.params.id)){
+            return true
+        }
+        else{
+            return false;   
+        }
+    })
+    res.render("empdetail",{
+        details:emp
+    })
+})
 module.exports = router;
--- std_app.js
+++ std_app.js
@@ -4,7 +4,11 @@ var stdrouter = require('./student_router');
 var emprouter = require("./employee_router")
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
-
+app.use(express.static('open'));
+app.use("/student",function(res,req,next){
+    console.log("i am middleware");
+    next();
+})
 app.set('view engine', 'pug');
 app.set('views','./views');
 
@@ -12,9 +16,13 @@ app.get("/",function(req,res){
    res.sendFile(__dirname+"/stdhome.html")
 })
 
-app.use('/student', stdrouter)
+app.use('/student',stdrouter)
 app.use("/employee",emprouter)
 
+app.get('/abc',function(req,res){
+    console.log(req.params);
+    res.send("i am working")
+})
 app.listen(7080,function()
 {
     console.log("listening on 7080")
